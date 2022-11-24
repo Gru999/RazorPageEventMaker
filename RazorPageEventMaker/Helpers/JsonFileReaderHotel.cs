19d31a79d0c1814c4c367ceb4b163c66ce549418
Frederik Baun Grubak <fred00s7@edu.zealand.dk>
2022-11-24 14:16:33 +0100

    -

--- RazorPageEventMaker/Helpers/JsonFileReaderHotel.cs
+++ RazorPageEventMaker/Helpers/JsonFileReaderHotel.cs
@@ -1,6 +1,13 @@
-﻿namespace RazorPageEventMaker.Helpers
-{
-    public class JsonFileReaderHotel
-    {
+﻿using RazorPageEventMaker.Models;
+using System.Text.Json;
+
+namespace RazorPageEventMaker.Helpers {
+    public class JsonFileReaderHotel {
+        public static List<Hotel> ReadJson(string JsonFileName) {
+            using (var jsonFileReader = File.OpenText(JsonFileName)) {
+                string indata = jsonFileReader.ReadToEnd();
+                return JsonSerializer.Deserialize<List<Hotel>>(indata);
+            }
+        }
     }
 }
--- RazorPageEventMaker/Services/JsonHotelRepository.cs
+++ RazorPageEventMaker/Services/JsonHotelRepository.cs
@@ -1,6 +1,61 @@
-﻿namespace RazorPageEventMaker.Services
+﻿using RazorPageEventMaker.Helpers;
+using RazorPageEventMaker.Interfaces;
+using RazorPageEventMaker.Models;
+
+namespace RazorPageEventMaker.Services
 {
-    public class JsonHotelRepository
-    {
+    public class JsonHotelRepository : IHotelRepository {
+        string JsonFileName = @"C:\Users\Grubak\OneDrive\Documents\Datamatiker Martriale\Code\RazorPageEventMaker\RazorPageEventMaker\JsonData\JsonHotels.json";
+        public void AddHotel(Hotel ho) {
+            List<Hotel> hotels = GetAllHotel().ToList();
+            List<int> hotelIds = new List<int>();
+            foreach (var htl in hotels) {
+                hotelIds.Add(htl.Id);
+            }
+            if (hotelIds.Count != 0) {
+                int start = hotelIds.Max();
+                ho.Id = start + 1;
+            } else {
+                ho.Id = 1;
+            }
+            hotels.Add(ho);
+            JsonFileWriterHotel.WritetoJson(hotels, JsonFileName);
+        }
+
+        public List<Hotel> GetAllHotel() {
+            return JsonFileReaderHotel.ReadJson(JsonFileName);
+        }
+
+        public Hotel GetHotel(int id) {
+            List<Hotel> hotels = GetAllHotel();
+            foreach (var h in hotels) {
+                if (h.Id == id) {
+                    return h;
+                }
+            }
+            return new Hotel();
+        }
+
+        public void UpdateHotel(Hotel ho) {
+            List<Hotel> hotels = GetAllHotel().ToList();
+            if (ho != null) {
+                foreach (Hotel h in hotels) {
+                    if (h.Id == ho.Id) {
+                        h.Id = ho.Id;
+                        h.Name = ho.Name;
+                        h.Address = ho.Address;
+                    }
+                }
+                JsonFileWriterHotel.WritetoJson(hotels, JsonFileName);
+            }
+        }
+
+        public void DeleteHotel(int id) {
+            List<Hotel> hotels = GetAllHotel().ToList();
+            Hotel deleteHotel = GetHotel(id);
+            hotels.Remove(deleteHotel);
+            JsonFileWriterHotel.WritetoJson(hotels, JsonFileName);
+
+        }
     }
 }
