37ea2cd1215cf4fca50fa9bab3303e78e25db7f0
Frederik Baun Grubak <fred00s7@edu.zealand.dk>
2022-11-24 14:23:03 +0100

    -

--- RazorPageEventMaker/Helpers/JsonFileWriterHotel.cs
+++ RazorPageEventMaker/Helpers/JsonFileWriterHotel.cs
@@ -1,6 +1,17 @@
-﻿namespace RazorPageEventMaker.Helpers
-{
-    public class JsonFileWriterHotel
-    {
+﻿using RazorPageEventMaker.Models;
+using System.Text.Json;
+
+namespace RazorPageEventMaker.Helpers {
+    public class JsonFileWriterHotel {
+        public static void WritetoJson(List<Hotel> hotels, string JsonFileName) {
+            //using(FileStream outputStream =File.OpenWrite(JsonFileName))
+            using (FileStream outputStream = File.Create(JsonFileName)) {
+                var writter = new Utf8JsonWriter(outputStream, new JsonWriterOptions {
+                    SkipValidation = false,
+                    Indented = true,
+                });
+                JsonSerializer.Serialize<Hotel[]>(writter, hotels.ToArray());
+            }
+        }
     }
 }
